c7b1f212e9bb801259d04b4c16369c6726b9bf19
David Koudela <dkoudela@volny.cz>
2015-02-26 04:25:13 -0800

    Changed DB schemas from ldif to schema format.

--- scripts/set-default-slapd.sh
+++ scripts/set-default-slapd.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+SLAPDCONF=`dirname $0`/../slapd.conf
+DATABASEDIR=`sed -n 's/^directory[ \t]*\(.*\)/\1/p' ${SLAPDCONF} `
+
+# Stop the LDAP service
 service slapd stop
+
+# Clean the LDAP database
+rm -f ${DATABASEDIR}/*
+cp `dirname $0`/../DB_CONFIG ${DATABASEDIR}
+slapd
+killall `which slapd`
+sudo chown -R ldap:ldap ${DATABASEDIR}
+
+# Setup the LDAP schema
 mkdir -p /etc/openldap/slapd.d.new
-slaptest -f `dirname $0`/../slapd.conf -F /etc/openldap/slapd.d.new
+slaptest -f ${SLAPDCONF} -F /etc/openldap/slapd.d.new
 chown ldap:ldap /etc/openldap/slapd.d.new -R
 chmod 700 /etc/openldap/slapd.d.new
 rm -Rf /etc/openldap/slapd.d
 mv /etc/openldap/slapd.d.new /etc/openldap/slapd.d
+
+# Start the LDAP service
 service slapd start
 
--- scripts/set-schema.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ldapmodify -D "cn=config" -y `dirname $0`/../passwdfile.conf -f `dirname $0`/../ldif/inetperson.schema.ldif
-ldapmodify -D "cn=config" -y `dirname $0`/../passwdfile.conf -f `dirname $0`/../ldif/core.schema.ldif
-
